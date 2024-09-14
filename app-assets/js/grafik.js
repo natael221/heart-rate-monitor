9a28c8f039345346fec54d58317b413f5317e1ca
Natanael Siwalette <luckynutz648@gmail.com>
2024-09-14 12:26:52 +0700

    Menambahkan folder besar

---
+++ app-assets/js/grafik.js
@@ -0,0 +1,21 @@
+var grafikdata = echarts.init(
+    document.getElementById("grafik")
+)
+var option = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        smooth: true
+      }
+    ]
+  };
+
+grafikdata.setOption(option)
